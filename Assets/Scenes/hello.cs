d0c2ea441ec48e0822e12ada8f448d2f91fe2e22
hg-liam <liam.lepinay@homagames.com>
2026-04-10 15:20:58 +0200

    Remove nonsense

--- Assets/Scenes/hello.cs
+++ Assets/Scenes/hello.cs
@@ -7,7 +7,7 @@ public class hello : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gfzeukgvzuieghui
+        
     }
 
     // Update is called once per frame
