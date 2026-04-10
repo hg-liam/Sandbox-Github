b4daf809445c12a07a6fdb73f7b2691bf0562391
hg-liam <liam.lepinay@homagames.com>
2026-04-10 15:08:39 +0200

    Blabla

---
+++ Assets/Scenes/Blabla.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blabla : MonoBehaviour
+{
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+}
